3e3af81364a4a858c1175536077088d3fdf91e6b
Harriet <harriet.hall@madetech.com>
2024-07-29 13:45:30 +0100

    display title & refactor 'Properties : 4' into a footer tag to prevent conflict with other tags

--- tests/test_num_of_props.py
+++ tests/test_num_of_props.py
@@ -20,9 +20,14 @@ def chrome_browser():
 def test_displays_num_of_properties(chrome_browser):
 
     chrome_browser.get("http://127.0.0.1:5000")
-    body_tag = chrome_browser.find_element(By.TAG_NAME, "body")
-    regex_find_num_of_properties = re.search(r"\d+", body_tag.text)
+    footer_tag = chrome_browser.find_element(By.TAG_NAME, "footer")
+    regex_find_num_of_properties = re.search(r"\d+", footer_tag.text)
     number_of_properties = int(regex_find_num_of_properties.group())
-
-    assert body_tag.text == "Properties : 4"
+    assert footer_tag.text == "Properties : 4"
     assert number_of_properties == 4
+    
+def test_displays_main_title(chrome_browser):
+    chrome_browser.get("http://127.0.0.1:5000")
+    h1_tag = chrome_browser.find_element(By.TAG_NAME, "h1")
+    assert h1_tag.text == "Hard to Heat Homes"
+    
