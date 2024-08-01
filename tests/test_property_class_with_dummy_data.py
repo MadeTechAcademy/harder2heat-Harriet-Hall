2f2eaa13cfd65333900c57acee69f745d27708f6
Harriet <harriet.hall@madetech.com>
2024-08-01 17:45:22 +0100

    test property class gas correct attributes from dummy data

---
+++ tests/test_property_class_with_dummy_data.py
@@ -0,0 +1,46 @@
+from src.property import Property
+import json
+
+with open("../properties.json", "r") as data:
+    dummy_data = json.load(data)
+    dummy_data_properties = dummy_data[0]["properties"]
+    dummy_property = Property(dummy_data_properties["uprnreference"][0]["uprn"])
+
+
+def test_property_class_has_correct_attributes_from_dummy_data():
+    dummy_property.connectivity = dummy_data_properties["connectivity"]
+    dummy_property.year = dummy_data_properties["buildingage_year"]
+    dummy_property.material = dummy_data_properties["constructionmaterial"]
+    coordinates_list = dummy_data[0]["geometry"]["coordinates"][0]
+    dummy_property.coordinates = coordinates_list
+    dummy_property.long = coordinates_list[0][0]
+    dummy_property.lat = coordinates_list[0][1]
+    dummy_property.size = dummy_data_properties["geometry_area_m2"]
+    dummy_property.osid = dummy_data_properties["osid"]
+    dummy_property.age_updated_date = dummy_data_properties["buildingage_updatedate"]
+    
+    
+    assert dummy_property.uprn == 100090062842
+    assert dummy_property.year == "None"
+    assert dummy_property.connectivity == "Semi-Connected"
+    assert dummy_property.material == "Brick Or Block Or Stone"
+    assert dummy_property.coordinates == [
+        [0.0452889, 52.4569136],
+        [0.0453246, 52.4569215],
+        [0.045323, 52.4569242],
+        [0.0453908, 52.4569392],
+        [0.0454023, 52.4569199],
+        [0.0455166, 52.4569451],
+        [0.0455493, 52.4568931],
+        [0.0453653, 52.4568526],
+        [0.0453603, 52.456861],
+        [0.0453246, 52.4568531],
+        [0.0452889, 52.4569136],
+    ]
+ 
+    assert dummy_property.long == 0.0452889
+    assert dummy_property.lat == 52.4569136
+    assert dummy_property.age_updated_date == "2024-05-20"
+    assert dummy_property.size == 111.601
+    assert dummy_property.osid == "02ae4ae4-6119-4d72-aef9-e56013d25e0d"
+    
