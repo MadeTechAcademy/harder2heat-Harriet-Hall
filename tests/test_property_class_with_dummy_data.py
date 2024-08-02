fae046ba2c8c786b61e9393ba0a2a9f3429b0378
Harriet <harriet.hall@madetech.com>
2024-08-02 13:34:08 +0100

    test handle_connectivity() standalone becomes Free-Standing

--- tests/test_property_class_with_dummy_data.py
+++ tests/test_property_class_with_dummy_data.py
@@ -5,11 +5,11 @@ import json
 with open("./properties.json", "r") as data:
     dummy_data = json.load(data)
     dummy_data_property = dummy_data[0]["properties"]
- 
-  
+
+
 @pytest.fixture
 def dummy_property():
-    property = Property(dummy_data_property["uprnreference"][0]["uprn"])    
+    property = Property(dummy_data_property["uprnreference"][0]["uprn"])
     yield property
 
 
@@ -24,8 +24,7 @@ def test_property_class_has_correct_attributes_from_dummy_data(dummy_property):
     dummy_property.size = dummy_data_property["geometry_area_m2"]
     dummy_property.osid = dummy_data_property["osid"]
     dummy_property.age_updated_date = dummy_data_property["buildingage_updatedate"]
-    
-    
+
     assert dummy_property.uprn == 100090062842
     assert dummy_property.year == "None"
     assert dummy_property.connectivity == "Semi-Connected"
@@ -43,43 +42,45 @@ def test_property_class_has_correct_attributes_from_dummy_data(dummy_property):
         [0.0453246, 52.4568531],
         [0.0452889, 52.4569136],
     ]
- 
 
     assert dummy_property.age_updated_date == "2024-05-20"
     assert dummy_property.size == 111.601
     assert dummy_property.osid == "02ae4ae4-6119-4d72-aef9-e56013d25e0d"
-    
- 
+
+
 def test_handle_year_string_with_buildingage_year_as_None(dummy_property):
     dummy_property.year = dummy_data_property["buildingage_year"]
     assert dummy_property.year == "None"
     dummy_property.handle_year_string()
     assert dummy_property.year == 1959
 
+
 def test_handle_year_string_with_buildingage_year(dummy_property):
     dummy_property.year = 1999
     dummy_property.handle_year_string()
     assert dummy_property.year == 1999
- 
-    
+
+
 def test_handle_year_string_with_buildingage_year_is_period(dummy_property):
     dummy_property.year = dummy_data_property["buildingage_period"]
     assert dummy_property.year == "1980-1989"
     dummy_property.handle_year_string()
     assert dummy_property.year == 1989
 
-     
+
 def test_handle_connectivity_with_semi_connected(dummy_property):
     dummy_property.connectivity = dummy_data_property["connectivity"]
     assert dummy_property.connectivity == "Semi-Connected"
     dummy_property.handle_connectivity()
     assert dummy_property.connectivity == "Dual-Connected"
 
+
 dummy_end_connected = dummy_data[1]["properties"]
- 
+
+
 @pytest.fixture
 def dummy_property_1():
-    property = Property(dummy_end_connected["uprnreference"][0]["uprn"])    
+    property = Property(dummy_end_connected["uprnreference"][0]["uprn"])
     yield property
 
 
@@ -88,4 +89,19 @@ def test_handle_connectivity_with_end_connected(dummy_property_1):
     assert dummy_property_1.connectivity == "End-Connected"
     dummy_property_1.handle_connectivity()
     assert dummy_property_1.connectivity == "Single-Connected"
-    
+
+
+dummy_standalone = dummy_data[2]["properties"]
+
+
+@pytest.fixture
+def dummy_property_2():
+    property = Property(dummy_standalone["uprnreference"][0]["uprn"])
+    yield property
+
+
+def test_handle_connectivity_with_standalone(dummy_property_2):
+    dummy_property_2.connectivity = dummy_standalone["connectivity"]
+    assert dummy_property_2.connectivity == "Standalone"
+    dummy_property_2.handle_connectivity()
+    assert dummy_property_2.connectivity == "Free-Standing"
