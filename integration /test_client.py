0b75a15b62ca67305fe6957fd718b67929971019
Harriet <harriet.hall@madetech.com>
2024-08-06 19:09:04 +0100

    refactor: make mock data a attribute in the TestClient class

--- integration /test_client.py
+++ integration /test_client.py
@@ -1,12 +1,44 @@
 import unittest
 from unittest.mock import patch, MagicMock
 from client import get_council_properties_from_api
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+from council import Council
 
 
 class TestClient(unittest.TestCase):
     def setUp(self):
         self.url = "/council/properties"
-
+        self.mock_data = {
+            "data": [{
+                "geometry": {
+                    "coordinates": [
+                        [
+                            [0.0452889, 52.4569136],
+                        ]
+                    ]
+                },
+                "properties": {
+                    "osid": "123",
+                    "connectivity": "Semi-Connected",
+                    "uprnreference": [
+                        {
+                            "uprn": 1,
+                        }
+                    ],
+                    "buildingage_year": 1988,
+                    "geometry_area_m2": 11,
+                    "buildingage_period": "1980-1989",
+                    "constructionmaterial": "Brick Or Block Or Stone",
+                    "buildingage_updatedate": "2024-05-20",
+                    "number_of_floors" : 2,
+                    "distance_to_public_transport_meters" : 19
+                    
+                },
+            }],
+            "status": "ok",
+        }
     @patch("client.requests")
     def test_get_council_properties_from_api_returns_200(self, mock_requests):
 
@@ -34,43 +66,14 @@ class TestClient(unittest.TestCase):
 
         mock_response = MagicMock()
         mock_response.status_code = 200
-        mock_response.json.return_value = {
-            "data": {
-                "geometry": {
-                    "coordinates": [
-                        [
-                            [0.0452889, 52.4569136],
-                        ]
-                    ]
-                },
-                "properties": {
-                    "osid": "123",
-                    "connectivity": "Semi-Connected",
-                    "uprnreference": [
-                        {
-                            "uprn": 1,
-                        }
-                    ],
-                    "buildingage_year": 1988,
-                    "geometry_area_m2": 11,
-                    "buildingage_period": "1980-1989",
-                    "constructionmaterial": "Brick Or Block Or Stone",
-                    "buildingage_updatedate": "2024-05-20",
-                    "number_of_floors" : 2,
-                    "distance_to_public_transport_meters" : 19
-                    
-                },
-            },
-            "status": "ok",
-        }
+        mock_response.json.return_value = self.mock_data
         mock_requests.get.return_value = mock_response
 
         status, body = get_council_properties_from_api(self.url)
         self.assertTrue(status)
         self.assertEqual(body["status"], "ok")
-        self.assertEqual(body["data"]["properties"]["number_of_floors"], 2)
-        self.assertEqual(body["data"]["properties"]["distance_to_public_transport_meters"], 19)
-        
-
+        self.assertEqual(body["data"][0]["properties"]["number_of_floors"], 2)
+        self.assertEqual(body["data"][0]["properties"]["distance_to_public_transport_meters"], 19)
+    
 
 unittest.main()
