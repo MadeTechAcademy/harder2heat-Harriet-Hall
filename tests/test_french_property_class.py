fa6c80ad9167b85ea292d7b4906e75b05379d838
Harriet <harriet.hall@madetech.com>
2024-08-06 20:27:54 +0100

    test french property instance has correct attributed

--- tests/test_french_property_class.py
+++ tests/test_french_property_class.py
@@ -1,6 +1,22 @@
 from src.french_property import FrenchProperty
 from src.property import Property
 
+french_property = FrenchProperty(1, 0, "", "", [], 0, "", "", 5, 19)
+
 def test_french_propery_inherits_from_property_class():
-    french_property = FrenchProperty(1, 0, "", "", [], 0, "", "", 5, 19)
     assert isinstance(french_property, Property)
+    
+def test_french_propery_has_correct_attributes():
+    
+    assert french_property.uprn == 1
+    assert french_property.year == 0
+    assert french_property.connectivity == ""
+    assert french_property.material == ""
+    assert french_property.coordinates == []
+    assert french_property.size == 0
+    assert french_property.osid == ""
+    assert french_property.age_updated_date == ""
+    assert french_property.floors == 5
+    assert french_property.distance_to_transport == 19
+    assert french_property.score == 0
+
