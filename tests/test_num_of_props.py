21381363e381063cc756f7925ba67608095c4053
Harriet <harriet.hall@madetech.com>
2024-08-07 10:52:44 +0100

    refactor: remove repetition

--- tests/test_num_of_props.py
+++ tests/test_num_of_props.py
@@ -5,30 +5,30 @@ from selenium.webdriver.common.by import By
 
 
 @pytest.fixture()
-def chrome_browser():
+def browser():
     driver = webdriver.Chrome()
+    driver.get("http://127.0.0.1:5000")
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
+    
 
+def test_displays_num_of_properties(browser):
 
-def test_displays_num_of_properties(chrome_browser):
-
-    chrome_browser.get("http://127.0.0.1:5000")
-    footer_tag = chrome_browser.find_element(By.TAG_NAME, "footer")
+    footer_tag = browser.find_element(By.TAG_NAME, "footer")
     regex_find_num_of_properties = re.search(r"\d+", footer_tag.text)
     number_of_properties = int(regex_find_num_of_properties.group())
     assert footer_tag.text == "Properties : 4"
     assert number_of_properties == 4
     
-def test_displays_main_title(chrome_browser):
-    chrome_browser.get("http://127.0.0.1:5000")
-    h1_tag = chrome_browser.find_element(By.TAG_NAME, "h1")
+def test_displays_main_title(browser):
+
+    h1_tag = browser.find_element(By.TAG_NAME, "h1")
     assert h1_tag.text == "Hard to Heat Homes"
 
-def test_displays_table_with_propery_features_as_headers(chrome_browser):
-    chrome_browser.get("http://127.0.0.1:5000")
-    table = chrome_browser.find_element(By.ID, "properties-table")
+def test_displays_table_with_propery_features_as_headers(browser):
+  
+    table = browser.find_element(By.ID, "properties-table")
     headers = table.find_elements(By.TAG_NAME, "th")
    
     assert len(headers) == 9
