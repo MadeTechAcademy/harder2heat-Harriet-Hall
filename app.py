40bd1e2f08bd68c304d2038b622a94e4054fcfa4
Harriet <harriet.hall@madetech.com>
2024-08-07 15:13:08 +0100

    refactor: pass correct data into home.html

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ from flask import Flask, render_template
 from src.council import Council
 app = Flask(__name__)
 
-properties = None
+
 with open('properties.json') as json_properties:
    data = json.load(json_properties)
    council = Council("any", "UK")
-   properties = council.generate_property_class_list(data)
-
+   council.generate_property_class_list(data)
 @app.route("/")
 def home():
-   return render_template("home.html", properties=properties)
+   return render_template("home.html", properties=council.list_of_properties)
