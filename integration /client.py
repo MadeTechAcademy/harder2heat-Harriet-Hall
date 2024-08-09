13fdf2018c4b2ae9ba1c3a0c860f81235eaccd78
Harriet <harriet.hall@madetech.com>
2024-08-09 09:36:54 +0100

    remove integration folder

--- integration /client.py
+++
@@ -1,8 +0,0 @@
-
-import requests
-
-def get_council_properties_from_api(url):
-    res = requests.get(url)
-    if res.status_code == 200:
-        return (True, res.json())
-    return (False, None)
