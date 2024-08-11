fee302a001f7222a9991f4ca50ef03c80be38c2b
Harriet <harriet.hall@madetech.com>
2024-08-11 16:54:04 +0100

    add exception handling

--- e2e testing/test_homepage.py
+++ e2e testing/test_homepage.py
@@ -2,6 +2,9 @@ import pytest
 import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 
 @pytest.fixture()
@@ -13,87 +16,122 @@ def browser():
     driver.quit()
 
 
-def test_displays_num_of_properties(browser):
+class TestHomePage:
 
-    footer_tag = browser.find_element(By.TAG_NAME, "footer")
-    regex_find_num_of_properties = re.search(r"\d+", footer_tag.text)
-    number_of_properties = int(regex_find_num_of_properties.group())
-    assert footer_tag.text == "Properties : 4"
-    assert number_of_properties == 4
+    def get_table(self, browser):
+        try:
+            return browser.find_element(By.ID, "data")
+        except NoSuchElementException:
+            pytest.fail("No table element found")
+            
+            
+    def test_displays_num_of_properties(self, browser):
+        try:
+            footer_tag = browser.find_element(By.TAG_NAME, "footer")
+            regex_find_num_of_properties = re.search(r"\d+", footer_tag.text)
+            number_of_properties = int(regex_find_num_of_properties.group())
+            assert footer_tag.text == "Properties : 4"
+            assert number_of_properties == 4
+        except NoSuchElementException:
+            pytest.fail("No footer element found")
 
-
-def test_displays_main_title(browser):
-
-    h1_tag = browser.find_element(By.TAG_NAME, "h1")
-    assert h1_tag.text == "Hard to Heat Homes"
-
-
-def test_displays_table_with_propery_features_as_headers(browser):
-    table = browser.find_element(By.ID, "data")
-    headers = table.find_elements(By.TAG_NAME, "th")
-    
-    assert len(headers) == 10
-    list_of_headers = []
-
-    for header in headers:
-        text = header.text
-        list_of_headers.append(text)
-
-    assert list_of_headers == [
-        "UPRN",
-        "OSID",
-        "Year Built",
-        "Updated on",
-        "Connectivity",
-        "Building materials",
-        "Size in m2",
-        "Coordinates",
-        "Hard To Heat Score:\n" "(easy) 0 - 4 (hard)",
-        "",
-    ]
+    def test_displays_main_title(self, browser):
+        try: 
+            h1_tag = browser.find_element(By.TAG_NAME, "h1")
+            assert h1_tag.text == "Hard to Heat Homes"
+        except NoSuchElementException:
+            pytest.fail("No h1 element found")
 
 
-def test_displays_table_property_data(browser):
-    table = browser.find_element(By.ID, "data")
-    cells = table.find_elements(By.TAG_NAME, "td")
-    cell_data_list = []
-    for cell in cells[0:9]:
-        text = cell.text
-        cell_data_list.append(text)
+    def test_displays_table_with_propery_features_as_headers(self, browser):
+        try:
+                
+            table = self.get_table(browser)
+            headers = table.find_elements(By.TAG_NAME, "th")
 
-    assert cell_data_list == [
-        "100090062297",
-        "0b1107e5-00f8-4d89-b6ae-67f0f98a6517",
-        "1918",
-        "2024-03-13",
-        "Free-Standing",
-        "Brick Or Block Or Stone",
-        "120.143",
-        "[[0.0471489, 52.4569721], [0.0472922, 52.4569964], [0.0473205, 52.4569337], [0.0473576, 52.45694], [0.0473707, 52.4569112], [0.0473336, 52.4569049], [0.0473464, 52.4568765], [0.0472657, 52.4568629], [0.0472642, 52.4568662], [0.0472467, 52.4568633], [0.0472399, 52.4568621], [0.0472175, 52.4569117], [0.0472229, 52.4569126], [0.0472151, 52.4569296], [0.0472085, 52.456944], [0.047165, 52.4569366], [0.0471489, 52.4569721]]",
-        "2",
-    ]
+            assert len(headers) == 10
+            list_of_headers = []
+
+            for header in headers:
+                text = header.text
+                list_of_headers.append(text)
+
+            assert list_of_headers == [
+                "UPRN",
+                "OSID",
+                "Year Built",
+                "Updated on",
+                "Connectivity",
+                "Building materials",
+                "Size in m2",
+                "Coordinates",
+                "Hard To Heat Score:\n" "(easy) 0 - 4 (hard)",
+                "",
+            ]
+        except NoSuchElementException:
+            pytest.fail("No table header element found")
 
 
-def test_displays_multiple_properties(browser):
-    table = browser.find_element(By.ID, "data")
-    rows = table.find_elements(By.TAG_NAME, "tr")
+    def test_displays_table_property_data(self, browser):
+        try:
+            
+            table = self.get_table(browser)
+            cells = table.find_elements(By.TAG_NAME, "td")
+            cell_data_list = []
+            for cell in cells[0:9]:
+                text = cell.text
+                cell_data_list.append(text)
 
-    assert len(rows) > 2
+            assert cell_data_list == [
+                "100090062297",
+                "0b1107e5-00f8-4d89-b6ae-67f0f98a6517",
+                "1918",
+                "2024-03-13",
+                "Free-Standing",
+                "Brick Or Block Or Stone",
+                "120.143",
+                "[[0.0471489, 52.4569721], [0.0472922, 52.4569964], [0.0473205, 52.4569337], [0.0473576, 52.45694], [0.0473707, 52.4569112], [0.0473336, 52.4569049], [0.0473464, 52.4568765], [0.0472657, 52.4568629], [0.0472642, 52.4568662], [0.0472467, 52.4568633], [0.0472399, 52.4568621], [0.0472175, 52.4569117], [0.0472229, 52.4569126], [0.0472151, 52.4569296], [0.0472085, 52.456944], [0.047165, 52.4569366], [0.0471489, 52.4569721]]",
+                "2",
+            ]
+        except NoSuchElementException:
+            pytest.fail("No table data element found")
+
+    def test_displays_multiple_properties(self, browser):
+        try:
+            table = self.get_table(browser)
+            rows = table.find_elements(By.TAG_NAME, "tr")
+
+            assert len(rows) > 2
+        except NoSuchElementException:
+            pytest.fail("No table rows found")
 
 
-def test_displays_see_more_details_button(browser):
-    table = browser.find_element(By.ID, "data")
-    button = table.find_element(By.TAG_NAME, "a")
+    def test_displays_see_more_details_button(self, browser):
+        try:
+            
+            table = self.get_table(browser)
+            button = table.find_element(By.TAG_NAME, "a")
 
-    assert button.text == "See more details"
-    assert button.get_attribute("style") == "color: black; text-decoration: none;"
+            assert button.text == "See more details"
+            assert button.get_attribute("style") == "color: black; text-decoration: none;"
+        except NoSuchElementException:
+            pytest.fail("No button element found")
 
-def test_button_navigates_to_property_page_with_uprn(browser):
-    table = browser.find_element(By.ID, "data")
-    button = table.find_element(By.TAG_NAME, "a")
-    url = button.get_attribute("href")
-    assert browser.current_url != url
-    
-    button.click()
-    
-    assert browser.current_url == url
+
+    def test_button_navigates_to_property_page_with_uprn(self, browser):
+        try:
+
+            table = self.get_table(browser)
+            button = table.find_element(By.TAG_NAME, "a")
+            url = button.get_attribute("href")
+            assert browser.current_url != url
+
+            button.click()
+            WebDriverWait(browser, 10).until(EC.url_to_be(url))
+
+            assert browser.current_url == url
+
+        except TimeoutException:
+            print("Navigation took too long.")
+        except AssertionError:
+            print("Wrong URL")
