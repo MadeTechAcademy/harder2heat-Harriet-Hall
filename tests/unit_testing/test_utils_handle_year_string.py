431a687b4806206e42b79f9598698bab3fd08c2a
Harriet Hall <harriet.hall@madetech.com>
2024-08-29 20:18:08 +0100

    move handle_year_string test into seperate test file

---
+++ tests/unit_testing/test_utils_handle_year_string.py
@@ -0,0 +1,21 @@
+from src.property import Property
+from src.utils import handle_year_string
+
+
+def test_handle_year_string_with_year_as_None():
+    year = "None"
+    property_year = handle_year_string(year)
+    assert property_year == 1959
+
+
+def test_handle_year_string_with_year_known():
+    year = 1999
+    property_year = handle_year_string(year)
+    assert property_year == 1999
+
+
+def test_handle_year_string_with_buildingage_period():
+    year = "1980-1989"
+    property_year = handle_year_string(year)
+    assert property_year == 1989
+
