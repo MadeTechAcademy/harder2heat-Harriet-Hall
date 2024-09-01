ac9365c838dc029f756345eeb45dd71b9a959cb6
Harriet Hall <harriet.hall@madetech.com>
2024-09-01 20:21:28 +0100

    replace PropertyManager with SortProperties in app.py

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 import json
 from flask import Flask, render_template
-from src.property_manager import PropertyManager
 from src.generate_properties import GenerateProperties
-
+from src.utils import SortProperties
 app = Flask(__name__)
 
 with open('properties.json') as json_properties:
    data = json.load(json_properties)
    properties = GenerateProperties.generate_property_class_list(data)
-   ordered_properties = PropertyManager.get_hardest_to_heat_properties(properties)
+   
+   for property in properties:
+      property.calculate_score()
+
+   sorted_properties = SortProperties.get_hardest_to_heat_properties(properties) 
    
 @app.route("/")
 def home():
-   return render_template("home.html", properties=ordered_properties)
+   return render_template("home.html", properties=sorted_properties)
 
 @app.route("/<int:uprn>")
 def property(uprn):
     property = None
-    for property in properties:
-        if property.uprn == uprn:
-            property = property
+    for property_instance in properties:
+        if property_instance.uprn == uprn:
+            property = property_instance
 
     return render_template("property.html", property=property)
