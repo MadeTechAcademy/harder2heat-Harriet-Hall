1f9f2f275905758a629d4426fea6952b0f50669d
Harriet Hall <harriet.hall@madetech.com>
2024-09-01 20:19:42 +0100

    test SortProperties with multiple properties

--- tests/unit_testing/test_sort_properties_class.py
+++ tests/unit_testing/test_sort_properties_class.py
@@ -25,3 +25,16 @@ def test_get_hardest_to_heat_properties_with_one_property():
     assert result[0].score == 1
     assert len(result) == 1
     
+
+def test_get_hardest_to_heat_properties_with_multiple_properties():
+    
+    properties = [Property(1, 2000, "End-Connected", "", [], 0, "", ""), Property(1, "None", "End-Connected", "", [], 0, "", "")]
+    
+    for property in properties:
+        property.calculate_score()
+       
+
+    result = SortProperties.get_hardest_to_heat_properties(properties)
+
+    assert result[0].score >= result[1].score  
+    assert len(result) == 2
